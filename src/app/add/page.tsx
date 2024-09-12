1e12182c8a696418691565c56f3795490424e6a6
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-09-12 17:08:06 +0900

    add: plus icon

--- src/app/add/page.tsx
+++ src/app/add/page.tsx
@@ -1,22 +1,18 @@
 import * as Form from "@radix-ui/react-form";
 import clsx from "clsx";
-import Link from "next/link";
+import Header from "../components/header";
 
 export default function Add() {
   return (
     <>
-      <header className={clsx("px-5 py-4")}>
-        <h1>
-          <Link className={clsx("text-lg font-semibold")} href={"/"}>
-            一問一答メーカー
-          </Link>
-        </h1>
-      </header>
-      <main className={clsx("px-5")}>
+      <Header />
+      <main className={clsx("pl-1.5 pr-3")}>
         <Form.Root>
-          <Form.Field className={clsx("mb-3 flex gap-3")} name="question">
+          <Form.Field className={clsx("mb-2 flex gap-1")} name="question">
             <div className={clsx("grid place-items-center")}>
-              <Form.Label className="question">問題</Form.Label>
+              <Form.Label className="question rounded bg-gray-800 px-3 py-2 text-white">
+                問題
+              </Form.Label>
             </div>
             <div className={clsx("flex-grow")}>
               <Form.Control asChild>
@@ -36,9 +32,9 @@ export default function Add() {
               </Form.Message>
             </div>
           </Form.Field>
-          <Form.Field className={clsx("mb-4 flex gap-3")} name="answer">
+          <Form.Field className={clsx("mb-4 flex gap-1")} name="answer">
             <div className={clsx("grid place-items-center")}>
-              <Form.Label className="answer">解答</Form.Label>
+              <Form.Label className={clsx("answer px-3 py-2")}>解答</Form.Label>
             </div>
             <div className={clsx("flex-grow")}>
               <Form.Control asChild>
@@ -59,7 +55,7 @@ export default function Add() {
             </div>
           </Form.Field>
           <Form.Submit asChild>
-            <div className={clsx("pl-11")}>
+            <div className={clsx("pl-[60px]")}>
               <button
                 className={clsx("rounded bg-orange-500 px-10 py-2 text-white")}
               >
