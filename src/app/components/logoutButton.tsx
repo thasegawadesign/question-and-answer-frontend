e54a2cbf2bcf7cee1b4c03a6eabce6f1e15f36e2
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-09-19 17:26:52 +0900

    add: logout btn

---
+++ src/app/components/logoutButton.tsx
@@ -0,0 +1,19 @@
+"use client";
+
+import clsx from "clsx";
+import { signOut } from "next-auth/react";
+
+export default function LogoutButton() {
+  return (
+    <>
+      <button
+        onClick={() => signOut()}
+        className={clsx(
+          "flex items-center gap-1 rounded bg-red-600 px-4 py-2.5 text-white"
+        )}
+      >
+        <div className={clsx("block flex-shrink-0 text-sm")}>ログアウト</div>
+      </button>
+    </>
+  );
+}
