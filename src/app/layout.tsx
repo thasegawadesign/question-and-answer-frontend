f279d6b2ed2973f9f1e5f06517ac0d7f0bf2ca45
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-09-19 16:08:39 +0900

    fix: lang ja

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -18,7 +18,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html lang="ja">
       <body className={clsx(notoSansJP.className)}>{children}</body>
     </html>
   );
