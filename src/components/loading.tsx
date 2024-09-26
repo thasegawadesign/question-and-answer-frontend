68581ae6796612af96ac1c7029166e118ace366b
thasegawakaihatsu <hasegawatatsuya206@gmail.com>
2024-09-27 00:40:07 +0900

    fix: position loading

--- src/components/loading.tsx
+++ src/components/loading.tsx
@@ -2,7 +2,7 @@ export default function Loading() {
   return (
     <>
       <div
-        className="flex h-[calc(100vh-120px)] items-center justify-center"
+        className="fixed inset-0 flex items-center justify-center"
         aria-label="読み込み中"
       >
         <div className="h-10 w-10 animate-spin rounded-full border-4 border-orange-500 border-t-transparent"></div>
